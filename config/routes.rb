0b43547cc9cb4bf6c9f5f7eae7d8651d35e925ae
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-07 10:11:06 -0400

    Comment functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
   get "articles/:id", to: "articles#show"
